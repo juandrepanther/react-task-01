b1ba0cb78eb7d3a5157f3680a543fe20591a0a0a
juandrepanther <juandre@inbox.lv>
2021-06-10 21:31:03 +0300

    updated styles on Table

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -8,51 +8,32 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
 `
-const Table = styled.table`
-  border: 1px rgba(99, 99, 99, 0.2) solid;
-  border-collapse: collapse;
 
-  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-  width: 300px;
-  margin: 0;
-  padding: 0;
-`
-const Headers = styled.thead`
-  background-color: #7ed6df;
-  margin: 0;
-  padding: 0;
-`
-const Items = styled.tbody`
-  &:nth-child(even) {
-    background-color: #f2f2f2;
-  }
-  /* &:last-child {
-    border-bottom-left-radius: 10px;
-  } */
-`
+
+
 function TransactionHistory(props) {
   return (
     <Wrapper>
-      <Table>
-        <Headers>
+      <table>
+        <thead>
           <tr>
             <th>Type</th>
             <th>Amount</th>
             <th>Currency</th>
           </tr>
-        </Headers>
+        </thead>
         {props.transactions.map((item) => {
           return (
-            <Items key={item.id}>
+            <tbody key={item.id}>
               <tr>
                 <td>{item.type}</td>
                 <td>{item.amount}</td>
                 <td>{item.currency}</td>
               </tr>
-            </Items>
+            </tbody>
           )
         })}
-      </Table>
+      </table>
     </Wrapper>
   )
 }
